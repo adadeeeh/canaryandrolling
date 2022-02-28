0eca3133251137860710b1697fbe28f2da5e9430
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2022-03-01 00:34:29 +0700

    change name

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "alb" {
 }
 
 resource "aws_lb_target_group" "http" {
-  name     = "target-group-${module.vpc.name}"
+  name     = "tg-${module.vpc.name}"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
