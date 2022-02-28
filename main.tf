d263fac9cec66de181a48330a003d2920d5f90e4
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2022-03-01 00:31:12 +0700

    add alb target group and listener

--- main.tf
+++ main.tf
@@ -55,3 +55,28 @@ module "alb" {
   subnets         = module.vpc.public_subnets
   security_groups = [module.lb_security_group.security_group_id]
 }
+
+resource "aws_lb_target_group" "http" {
+  name     = "target-group-${module.vpc.name}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+    timeout  = 5
+    interval = 10
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = module.alb.lb_arn
+  port              = "80"
+  protocol          = "http"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
