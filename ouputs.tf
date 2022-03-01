15ee548ee197703ddfa09bfcde60b2611c218d31
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2022-03-01 14:41:18 +0700

    change module name and add output

---
+++ ouputs.tf
@@ -0,0 +1,19 @@
+output "aws_region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "public_subnets" {
+  description = "Public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "web_sg_id" {
+  description = "Security Group"
+  value       = module.web_sg.security_group_id
+}
+
+output "target_group_arn" {
+  description = "ALB target group ARN"
+  value       = aws_lb_target_group.http.arn
+}
